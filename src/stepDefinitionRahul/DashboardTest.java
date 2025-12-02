21e8addd81b96ca6f97b9600cd5b269e1c81e526
Marwa <marwahelmy151@gmail.com>
2025-12-02 14:38:16 +0200

    Update selected files

--- src/stepDefinitionRahul/DashboardTest.java
+++ src/stepDefinitionRahul/DashboardTest.java
@@ -21,20 +21,22 @@ public class DashboardTest extends TestBase {
 	ShoppingcartPage shoppingcartobject;
 	Checkoutpage checkoutobject;
 
-	@And("^User can select product (.+)$")
-	public void User_can_select_product(String productcategory) throws InterruptedException, IOException {
-
+	@And("^User can select product (.+) and (.+) from list and select (.+)$")
+	public void User_can_select_product(String productcategory, String item, String desiredproduct)
+			throws InterruptedException, IOException {
+		homeobject = new HomePage(driver);
+		homeobject.hovergenericElement(productcategory);
+		homeobject.selectItemfromList(item);
 		dashboardobject = new DashboradPage(driver);
-		dashboardobject.hoverelement();
-		dashboardobject.selectProduct(productcategory);
+		dashboardobject.selectProduct(desiredproduct);
 		dashboardobject.addtocard();
 		Thread.sleep(3000);
+
 		Assert.assertEquals(dashboardobject.message.getText(), "The product has been added to your shopping cart");
 	}
 
 	@And("^User can add desired product (.+) to cart$")
 	public void User_can_add_product_to_cart(String desiredproduct) throws InterruptedException {
-		homeobject = new HomePage(driver);
 		homeobject.shoppingcartPage();
 		shoppingcartobject = new ShoppingcartPage(driver);
 		Thread.sleep(3000);
